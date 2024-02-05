c165aea3f2b70396c32cdb3d22e944647067ea7f
Tudor <tudor@192.168.0.102>
2024-02-05 18:34:31 +0200

    Deleted empty taskInput, adjusted env for Vercel deployment

--- knexfile.ts
+++ knexfile.ts
@@ -4,7 +4,7 @@ import { Knex } from 'knex';
 
 const config: Knex.Config = {
   client: 'pg',
-  connection: process.env.DATABASE_URL,
+  connection: process.env.POSTGRES_URL + (process.env.NODE_ENV === 'development' ? '' : '?sslmode=require'),
   migrations: {
     extension: 'ts',
   },
--- src/app/taskInput/page.tsx
+++
@@ -1,2 +0,0 @@
-import React, { useState, FormEvent } from 'react';
-
