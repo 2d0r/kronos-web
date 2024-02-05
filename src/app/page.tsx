7064255c816981d923f75bc1c4abaf24ff5febec
Tudor <tudor@192.168.0.102>
2024-02-05 18:11:30 +0200

    Add page to read and generate tasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
-import Image from 'next/image'
+import Button from "@/components/Button";
+import db from "@/modules/db";
+import { revalidatePath } from "next/cache";
 
-export default function Home() {
-  return <h1>Hello, Next.js!</h1>
+export default async function Home() {
+  const tasks = await db.task.findMany({orderBy: {createdAt: 'desc'}})
+
+  const generateTasks = async () => {
+    'use server'; 
+    
+    await db.task.createMany({
+      data: [
+        { title: 'Breakfast' },
+        { title: 'Lunch' },
+        { title: 'Dinner' } 
+      ]
+    })
+    revalidatePath('/');
+  }
+
+  return (
+    <main>
+      <Button onClick={generateTasks}>Generate Posts</Button>
+
+      {tasks.map((task) => (
+          <div key={task.id}>{task.title}</div>
+      ))}
+    </main>
+  );
 }
